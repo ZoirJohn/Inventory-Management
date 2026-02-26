35643d892ea321047752b1bb513237e30e6d7b22
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-02-26 09:37:30 +0500

    Enhance Inventory component to display creator name and creation date; update layout for better presentation

--- frontend/src/pages/Inventories.tsx
+++ frontend/src/pages/Inventories.tsx
@@ -4,12 +4,14 @@ import { NavLink, Outlet } from "react-router-dom";
 
 export default function Inventories() {
 	const { inventories } = useInventories();
+	console.log(inventories);
+
 	return (
 		<section>
 			<div className="container flex">
-				<div className="flex gap-4 items-center">
-					{inventories.map(({ title, description, id }) => (
-						<Inventory title={title} description={description} id={id} key={id} />
+				<div className="flex gap-4 items-center flex-wrap">
+					{inventories.map(({ title, description, id, creatorName, createdAt }) => (
+							<Inventory title={title} description={description} id={id} key={id} creatorName={creatorName} createdAt={createdAt} />
 					))}
 					<NavLink to="/create" className="size-12 bg-gray-300 flex items-center justify-center text-3xl! text-black">
 						+
