89e46333eb3ab55eb248e62f1bc238c364f791aa
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-03-05 22:14:20 +0500

    Change parameter type of createInventory function from 'unknown' to 'any' for flexibility in input handling

--- frontend/src/entities/utils/createInventory.ts
+++ frontend/src/entities/utils/createInventory.ts
@@ -1,6 +1,6 @@
 import { client } from "../client";
 
-export async function createInventory(body: unknown) {
+export async function createInventory(body: any) {
   const res = await client.CREATE_INVENTORY(body);
 
   return res;
